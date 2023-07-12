e0b3eab56b43366b9f66d324429ba1b9ded025cf
KirillLaptop <sinty.kpov@gmail.com>
2023-07-12 21:00:14 +0300

    final solution task "ads"

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,7 +1,17 @@
 const rotatorCase = Array.from(document.querySelectorAll('.rotator__case'));
+let index = 1;
 
 setInterval(function() {
     rotatorCase.forEach(element => {
-        element.classList.toggle('rotator__case_active');
+        if(element.classList.contains('rotator__case_active')) {
+            element.classList.toggle('rotator__case_active');
+        }
     });
+
+    if(index == 5) {
+        index = 1;
+    }
+    rotatorCase[index].classList.toggle('rotator__case_active');
+    index++
+
 }, 1000);
