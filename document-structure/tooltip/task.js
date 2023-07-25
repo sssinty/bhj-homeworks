0cdd21617053b0d66b8f670926c7103d7ce8d2f0
KirillLaptop <sinty.kpov@gmail.com>
2023-07-25 19:25:36 +0300

    revision task "tooltip"

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,9 +4,13 @@ const tooltip = document.querySelector(".tooltip");
 for(let element of hasTooltip) {
     element.addEventListener("click", (e) => {
         e.preventDefault();
-        tooltip.textContent = element.title;
-        tooltip.classList.add("tooltip_active");
-        tooltip.style.left = element.getBoundingClientRect().left + "px";
-        tooltip.style.top = element.getBoundingClientRect().top + 20  + "px";
+        if(tooltip.textContent == element.title){
+            tooltip.classList.toggle("tooltip_active");
+        }else if(tooltip.textContent != element.title) {
+            tooltip.textContent = element.title;
+            tooltip.classList.add("tooltip_active");
+            tooltip.style.left = element.getBoundingClientRect().left + "px";
+            tooltip.style.top = element.getBoundingClientRect().top + 20  + "px";
+        }
     }, false);
 }
